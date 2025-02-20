a40bfab529f56b08aa79e727c003dca3bd4e4142
Umutkaratutlu <107753189+Umutkaratutlu@users.noreply.github.com>
2025-02-20 23:24:00 +0300

    sayi

---
+++ sayilar/sayilar/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Bir sayı girin: ");
+double sayi;
+
+
+if (double.TryParse(Console.ReadLine(), out sayi))
+{
+    if (sayi > 0)
+    {
+        Console.WriteLine("Girilen sayı pozitiftir.");
+    }
+    else if (sayi < 0)
+    {
+        Console.WriteLine("Girilen sayı negatiftir.");
+    }
+    else
+    {
+        Console.WriteLine("Girilen sayı sıfırdır.");
+    }
+}
+else
+{
+    Console.WriteLine("Lütfen geçerli bir sayı girin!");
+}
